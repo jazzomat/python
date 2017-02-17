fbacccb7e0033e768994cfdabf82a8912718c19a
Jakob Abesser <nudelsalat@posteo.de>
2017-02-17 15:33:02 +0100

    Initialize folder for final project publication

---
+++ publications/book_2017/main.py
@@ -0,0 +1,7 @@
+__author__ = 'Jakob Abesser'
+
+""" This script allows to reproduce all experiment reported in the Jazzomat Research Project final publication 2017 """
+
+if __name__ == '__main__':
+
+    run_feature_extraction = True
